412b32c7076ac76acc1562eaadbb14302816b31b
Raghu Betina <raghu@firstdraft.com>
2018-11-16 18:07:35 +0000

    Whitelist columns in YogaFlow for ActiveAdmin

--- app/admin/yoga_flows.rb
+++ app/admin/yoga_flows.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register YogaFlow do
+
+ permit_params :name, :intention, :pose_id, :user_id, :description, :yoga_type
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #

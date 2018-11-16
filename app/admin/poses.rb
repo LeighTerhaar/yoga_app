0840a9bb5edb47a4d277124859d6e088175a33a4
Raghu Betina <raghu@firstdraft.com>
2018-11-16 18:07:31 +0000

    Whitelist columns in Pose for ActiveAdmin

--- app/admin/poses.rb
+++ app/admin/poses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Pose do
+
+ permit_params :english_name, :sanksrit, :sequence_id, :picture, :sun_a_id, :sun_b_id, :sun_c_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
